9ed0eafa106bb08a3358b5bc5845163b6472a444
Jane <iwof920125@qq.com>
2018-07-09 10:39:02 +0800

    测试Java对接kotlin
    修改配置使得编译通过

--- src/main/java/com/bzw/tars/server/jfgame/impl/TarsRoomServantImpl.java
+++ src/main/java/com/bzw/tars/server/jfgame/impl/TarsRoomServantImpl.java
@@ -1,10 +1,12 @@
 package com.bzw.tars.server.jfgame.impl;
 
 import com.bzw.tars.server.jfgame.TarsRoomServant;
+import com.bzw.tars.server.jfgame.kotlin.impl.Test;
 
 public class TarsRoomServantImpl implements TarsRoomServant {
     @Override
     public String hello() {
-        return String.format("hello kotlin, time=%s", System.currentTimeMillis());
+        Test test = new Test();
+        return String.format("hello ,%s, time=%s", test.hello(),  System.currentTimeMillis());
     }
 }
